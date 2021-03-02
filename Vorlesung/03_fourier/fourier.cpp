203f910bccacb267c622e173707c23d8c78b90e5
Felix Steinmetz <felix.steinmetz1@gmx.de>
2021-03-02 16:52:23 +0100

    Basic Setup Fourier Trafo

---
+++ Vorlesung/03_fourier/fourier.cpp
@@ -0,0 +1,13 @@
+#include "integration.h"
+#include <iostream>
+
+int main()
+{
+  int k; 
+  
+  // Anzahl an Fourier Koeffizienten
+  k = 3;
+  std::cout << "working" << std::endl;
+
+  return 0;
+}
