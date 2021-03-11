81883852afbae9029f6e6f0b34fa4134576f30f0
Felix Steinmetz <felix.steinmetz1@gmx.de>
2021-03-11 22:08:30 +0100

    Added gauss algorithm instead of gauss seidel since the matrix of least square method isnt necessarily diagonal dominant

--- Aufgaben/Aufgabe_4/leastsquares.cpp
+++ Aufgaben/Aufgabe_4/leastsquares.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 std::vector<double> gauss_seidel(std::vector<std::vector<double>> ARRM, std::vector<double> R);
 
+std::vector<double> gauss(std::vector<std::vector<double>> M,
+                          std::vector<double> R);
+
 int main()
 {
   double x, y, dx;
@@ -50,7 +53,7 @@ int main()
   }
   
   // Solve
-  std::vector<double> ARRA = gauss_seidel(ARRM,RHS);
+  std::vector<double> ARRA = gauss(ARRM,RHS);
   
   // output
   diskret = 101; // number of points for output
@@ -68,6 +71,42 @@ int main()
   }
 
   return 0;
+}
+
+std::vector<double> gauss(std::vector<std::vector<double>> M,
+                          std::vector<double> R) {
+
+  int n = R.size();
+  std::vector<double> X(n, 0.0);
+  int i, j, k;
+
+  //forward substitution
+  for (i = 0; i < n - 1; i++) {
+    for (k = i + 1; k < n; k++) {
+      for (j = i; j < n; j++) {
+        X[j] = -M[i][j] * M[k][i] / M[i][i];
+      }
+
+      R[k] = R[k] - R[i] * M[k][i] / M[i][i];
+
+      for (j = i; j <= n - 1; j++) {
+        M[k][j] = M[k][j] + X[j];
+      }
+    }
+  }
+
+  // backwards substitution
+  X[n - 1] = R[n - 1] / M[(n - 1)][n - 1];
+
+  for (i = n - 2; i >= 0; i--) {
+    X[i] = R[i];
+    for (j = i + 1; j < n; j++) {
+      X[i] = X[i] - M[i][j] * X[j];
+    }
+
+    X[i] = X[i] / M[i][i];
+  }
+  return X;
 }
 
 std::vector<double> gauss_seidel(std::vector<std::vector<double>> M, std::vector<double> R) {
