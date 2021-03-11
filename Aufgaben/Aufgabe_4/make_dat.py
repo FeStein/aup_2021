79f200f17949ed6a41e656785930088f138e1286
Felix Steinmetz <felix.steinmetz1@gmx.de>
2021-03-11 22:09:31 +0100

    changed input data to a bit more random points

--- Aufgaben/Aufgabe_4/make_dat.py
+++ Aufgaben/Aufgabe_4/make_dat.py
@@ -1,17 +1,16 @@
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from random import random
 
 x = np.linspace(0,1,26)
 rn = np.random.rand(len(x))*0.03
 rn[0] = 0
 rn[1] = 0
-#y =  4 * np.sqrt(x) - 3*x - 1 * x**2 + 2 * x**3 - 2 * x**4 + rn
-y =  4 * np.sqrt(x) - 3*x - 1 * x**2 + 2 * x**3 - 2 * x**4 
+y =  4 * np.sqrt(x) - 3*x - 1 * x**2 + 2 * x**3 - 2 * x**4 + rn
 
 
-#plt.plot(x,y,'o')
-#plt.show()
+plt.plot(x,y,'o')
+plt.show()
 
 with open('messpunkte.dat','w+') as f:
     for x,y in zip(x,y):
