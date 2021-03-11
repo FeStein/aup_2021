12f3df51e2aae76d396755518cdf9dc24bd766e1
Felix Steinmetz <felix.steinmetz1@gmx.de>
2021-03-11 16:42:50 +0100

    Aufgabe 5: Newton search for b not yet working

---
+++ Aufgaben/Aufgabe_5/a5.cpp
@@ -0,0 +1,129 @@
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+std::vector<double> gauss_seidel(std::vector<std::vector<double>> M,
+                                 std::vector<double> R);
+
+vector<double> ac_eq(vector<double> AX, vector<double> AY, double b);
+
+double dF(vector<double> AX, vector<double> AY, double a, double b, double c);
+
+int main() {
+  double x, y, b, dx, disk;
+
+  disk = 101; // number of x values for output
+  int m;
+
+  // read in the data
+  vector<double> AX;
+  vector<double> AY;
+
+  ifstream ifile;
+  ifile.open("IN.dat");
+  while (ifile >> x >> y) {
+    AX.push_back(x);
+    AY.push_back(y);
+  }
+  ifile.close();
+  m = AX.size();
+
+  double b1, b2, F1, F2, bk;
+
+  // intial guess of 2 b values
+  b1 = 0.5;
+  b2 = 0.3;
+  vector<double> KOEFF;
+
+  KOEFF = ac_eq(AX, AY, b1);
+  F1 = dF(AX, AY, KOEFF[0], b1, KOEFF[1]);
+
+  KOEFF = ac_eq(AX, AY, b2);
+  F2 = dF(AX, AY, KOEFF[0], b2, KOEFF[1]);
+
+  int max_iter = 20;
+
+  while (fabs(F2) > 0.001  && max_iter > 0) {
+    max_iter--;
+    bk = b2 - (F1 /(F1 - F2)) * (b2 - b1);
+    b1 = b2;
+    b2 = bk;
+    F1 = F2;
+    KOEFF = ac_eq(AX, AY, b2);
+    F2 = dF(AX, AY, KOEFF[0], b2, KOEFF[1]);
+    cout << bk << endl;
+  }
+
+  std::cout << "final b: " << b2 << std::endl;
+  // output
+  dx = (AX.back() - AX.front()) / (disk - 1);
+  ofstream ofile;
+  ofile.open("output");
+  for (int i = 0; i < disk; ++i) {
+    x = AX.front() + dx * i;
+    y = KOEFF[0] * pow(x, b2) + KOEFF[1];
+    ofile << x << " " << y << endl;
+  }
+
+  return 0;
+}
+
+double dF(vector<double> AX, vector<double> AY, double a, double b, double c) {
+  double out = 0.0;
+  for (int i = 0; i < AX.size(); ++i) {
+    out += (AY[i] - (a * pow(AX[i], b) + c)) * (a * pow(AX[i], b) * log(AX[i]));
+  }
+  return out;
+}
+
+// ac equation task c) returns {a c} (koefficients)
+vector<double> ac_eq(vector<double> AX, vector<double> AY, double b) {
+  double phi;
+  int m;
+  m = AX.size();
+
+  // create lse
+  vector<vector<double>> M(2, vector<double>(2, 0.0));
+  vector<double> B(2, 0.0);
+  for (int i = 0; i < m; ++i) {
+    phi = pow(AX[i], b);
+    M[0][0] += phi * phi;
+    M[0][1] += phi;
+    M[1][0] += phi;
+    M[1][1] += 1;
+    B[0] += AY[i] * phi;
+    B[1] += AY[i];
+  }
+
+  return gauss_seidel(M, B);
+}
+
+// gauss seidel algorithm
+std::vector<double> gauss_seidel(std::vector<std::vector<double>> M,
+                                 std::vector<double> R) {
+  int num_iter = 10000;
+  int n = R.size();
+  std::vector<double> X(n, 1.0);
+  std::vector<double> XBF(n, 1.0);
+
+  double sigma;
+  for (int iter = 0; iter < num_iter; iter++) {
+
+    for (int i = 0; i < n; i++) {
+      sigma = 0;
+      for (int j = 0; j < n; j++) {
+        if (i != j) {
+          sigma += M[i][j] * XBF[j];
+        }
+      }
+      X[i] = (1 / M[i][i]) * (R[i] - sigma);
+    }
+    for (int i = 0; i < n; i++) {
+      XBF[i] = X[i];
+    }
+  }
+  return X;
+}
---
+++ Aufgaben/Aufgabe_5/plot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+with open('IN.dat','r') as f: 
+    m_values = [[float(v) for v in l.strip().split()] for l in f]
+
+with open('output','r') as f: 
+    a_values = [[float(v) for v in l.strip().split(" ")] for l in f]
+
+m_x = [v[0] for v in m_values]
+m_y = [v[1] for v in m_values]
+
+a_x = [v[0] for v in a_values]
+a_y = [v[1] for v in a_values]
+
+
+plt.plot(m_x,m_y,'o')
+plt.plot(a_x,a_y,'-')
+plt.xlim(min(m_x), max(m_x))
+plt.show()
+
