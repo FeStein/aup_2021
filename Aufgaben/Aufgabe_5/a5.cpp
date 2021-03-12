9137a579551786e47580e01c17c78f6b167f2ad5
Felix Steinmetz <felix.steinmetz1@gmx.de>
2021-03-12 15:57:54 +0100

    cleanup

--- Aufgaben/Aufgabe_5/a5.cpp
+++ Aufgaben/Aufgabe_5/a5.cpp
@@ -3,26 +3,23 @@
 #include <math.h>
 #include <vector>
 
-using namespace std;
+std::vector<double> gauss(std::vector<std::vector<double>> M, std::vector<double> R);
 
-std::vector<double> gauss_seidel(std::vector<std::vector<double>> M,
-                                 std::vector<double> R);
+double dF(const std::vector<double> &AX, const std::vector<double> &AY, double a, double b, double c);
 
-vector<double> ac_eq(vector<double> AX, vector<double> AY, double b);
-
-double dF(vector<double> AX, vector<double> AY, double a, double b, double c);
+std::vector<double> ac_eq(const std::vector<double> &AX, const std::vector<double> &AY, double b);
 
 int main() {
-  double x, y, b, dx, disk;
+  double x, y, dx, disk;
 
   disk = 201; // number of x values for output
   int m;
 
   // read in the data
-  vector<double> AX;
-  vector<double> AY;
+  std::vector<double> AX;
+  std::vector<double> AY;
 
-  ifstream ifile;
+  std::ifstream ifile;
   ifile.open("IN.dat");
   while (ifile >> x >> y) {
     AX.push_back(x);
@@ -36,7 +33,7 @@ int main() {
   // intial guess of 2 b values
   b1 = -1.25;
   b2 = -1.0;
-  vector<double> KOEFF;
+  std::vector<double> KOEFF;
 
   KOEFF = ac_eq(AX, AY, b1);
   F1 = dF(AX, AY, KOEFF[0], b1, KOEFF[1]);
@@ -47,7 +44,7 @@ int main() {
   // This is some debug output
   dx = 10 / (disk - 1);
   double y2;
-  ofstream ofile;
+  std::ofstream ofile;
   ofile.open("dF");
   for (int i = 0; i < disk; ++i) {
     x = -5 + dx * i;
@@ -57,7 +54,7 @@ int main() {
     for (int i = 0; i < m; ++i) {
       y2 += pow(AY[i] - KOEFF[0] * pow(AX[i],x) - KOEFF[1],2) ;
     }
-    ofile << x << " " << y << " " << y2 << endl;
+    ofile << x << " " << y << " " << y2 << std::endl;
   }
   ofile.close();
 
@@ -72,7 +69,7 @@ int main() {
     F1 = F2;
     KOEFF = ac_eq(AX, AY, b2);
     F2 = dF(AX, AY, KOEFF[0], b2, KOEFF[1]);
-    cout << bk << endl;
+    std::cout << bk << std::endl;
   }
 
   std::cout << "final b: " << b2 << std::endl;
@@ -85,14 +82,14 @@ int main() {
   for (int i = 0; i < disk; ++i) {
     x = AX.front() + dx * i;
     y = KOEFF[0] * pow(x, b2) + KOEFF[1];
-    ofile << x << " " << y << endl;
+    ofile << x << " " << y << std::endl;
   }
   ofile.close();
 
   return 0;
 }
 
-double dF(vector<double> AX, vector<double> AY, double a, double b, double c) {
+double dF(const std::vector<double> &AX, const std::vector<double> &AY, double a, double b, double c) {
   double out = 0.0;
   for (int i = 0; i < AX.size(); ++i) {
     out += (AY[i] - (a * pow(AX[i], b) + c)) * (a * pow(AX[i], b) * log(AX[i]));
@@ -101,14 +98,14 @@ double dF(vector<double> AX, vector<double> AY, double a, double b, double c) {
 }
 
 // ac equation task c) returns {a c} (koefficients)
-vector<double> ac_eq(vector<double> AX, vector<double> AY, double b) {
+std::vector<double> ac_eq(const std::vector<double> &AX, const std::vector<double> &AY, double b) {
   double phi;
   int m;
   m = AX.size();
 
   // create lse
-  vector<vector<double>> M(2, vector<double>(2, 0.0));
-  vector<double> B(2, 0.0);
+  std::vector<std::vector<double>> M(2, std::vector<double>(2, 0.0));
+  std::vector<double> B(2, 0.0);
   for (int i = 0; i < m; ++i) {
     phi = pow(AX[i], b);
     M[0][0] += phi * phi;
@@ -119,32 +116,40 @@ vector<double> ac_eq(vector<double> AX, vector<double> AY, double b) {
     B[1] += AY[i];
   }
 
-  return gauss_seidel(M, B);
+  return gauss(M, B);
 }
 
-// gauss seidel algorithm
-std::vector<double> gauss_seidel(std::vector<std::vector<double>> M,
-                                 std::vector<double> R) {
-  int num_iter = 10000;
+std::vector<double> gauss(std::vector<std::vector<double>> M, std::vector<double> R) {
+
   int n = R.size();
-  std::vector<double> X(n, 1.0);
-  std::vector<double> XBF(n, 1.0);
+  std::vector<double> X(n, 0.0);
+  int i, j, k;
 
-  double sigma;
-  for (int iter = 0; iter < num_iter; iter++) {
-
-    for (int i = 0; i < n; i++) {
-      sigma = 0;
-      for (int j = 0; j < n; j++) {
-        if (i != j) {
-          sigma += M[i][j] * XBF[j];
-        }
+  //forward substitution
+  for (i = 0; i < n - 1; i++) {
+    for (k = i + 1; k < n; k++) {
+      for (j = i; j < n; j++) {
+        X[j] = -M[i][j] * M[k][i] / M[i][i];
       }
-      X[i] = (1 / M[i][i]) * (R[i] - sigma);
+
+      R[k] = R[k] - R[i] * M[k][i] / M[i][i];
+
+      for (j = i; j <= n - 1; j++) {
+        M[k][j] = M[k][j] + X[j];
+      }
     }
-    for (int i = 0; i < n; i++) {
-      XBF[i] = X[i];
+  }
+
+  // backwards substitution
+  X[n - 1] = R[n - 1] / M[(n - 1)][n - 1];
+
+  for (i = n - 2; i >= 0; i--) {
+    X[i] = R[i];
+    for (j = i + 1; j < n; j++) {
+      X[i] = X[i] - M[i][j] * X[j];
     }
+
+    X[i] = X[i] / M[i][i];
   }
   return X;
 }
