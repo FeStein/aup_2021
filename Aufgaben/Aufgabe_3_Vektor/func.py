af59a599197a60428d5e4d40025723ecf39f40bc
FelSt <63238040+FeStein@users.noreply.github.com>
2021-03-13 21:38:35 +0100

    Proper python script to generate fourier input data

--- Aufgaben/Aufgabe_3_Vektor/func.py
+++ Aufgaben/Aufgabe_3_Vektor/func.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-X = np.linspace(0, 2, 1000)
+X = np.linspace(0, 1, 1001)
 Y = - np.tan( np.pi * X - np.pi / 2)
+
+Y[0] = 5
 
 border = 15
 check = False
@@ -11,6 +13,7 @@ for index in range(X.size):
         check = True
     if Y[index] < border and check:
         break
+
 X = X[index:]
 Y = Y[index:]
 
@@ -21,16 +24,17 @@ for index in range(X.size):
 X = X[:index - 1]
 Y = Y[:index - 1]
 
-X_final = np.append(X, X + X[-1])
-Y_final = np.append(Y, Y)
+X = np.concatenate(([0],X))
+Y = np.concatenate(([0],Y))
 
-X_final = np.append(X_final, X + X_final[-1])
-Y_final = np.append(Y_final, Y)
+X_final = np.concatenate((X,X+1,X+2,[3]))
+Y_final = np.concatenate((Y,Y,Y,[0]))
 
 plt.plot(X_final, Y_final)
 plt.ylim(-20,20)
+plt.xlim(0,3)
 plt.show()
 
-#with open("signal.dat", "w+") as file:
-#    for line in range(X_final.size):
-#        file.write(f"{X_final[line]}\t{Y_final[line]}\n")
+with open("signal.dat", "w+") as file:
+    for line in range(X_final.size):
+        file.write(f"{X_final[line]} {Y_final[line]}\n")
